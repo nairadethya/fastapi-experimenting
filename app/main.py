015cac516d391527f1196636ed0cd3f25faefbe4
nairadethya2208 <88815815+nairadethya2208@users.noreply.github.com>
2023-01-13 13:43:25 +0530

    just minor updates

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
+from .library.helpers import *
 
 app = FastAPI()
 
@@ -11,16 +12,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    data = {
-        "page":"Home page"
-    }
+    data = openfile("home.md")
     return templates.TemplateResponse("page.html", {"request":request, "data": data})
 
 @app.get("/page/{page_name}", response_class=HTMLResponse)
 async def page(request: Request, page_name: str):
-
-    data = {
-        "page":page_name
-    }
-
+    data = openfile(page_name+".md")
     return templates.TemplateResponse("page.html", {"request":request, "data": data})
---
+++ app/pages/home.md
@@ -0,0 +1,2 @@
+# Hey there 👋, I am Adethya Nair. 
+Part time coder, full time GSW fan. 
